0bac6ff9ad7d2db078192159795c61a09f55d6b0
KamaKAzii <spskamakazii@gmail.com>
2012-06-14 21:45:43 +1000

    Initial commit with Rails app and postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ipsmobile::Application.config.secret_token = '79ed21b70a64c9f89ebfcf7f27de98d41c68ddef55973b24978737f7e8b2bee6fb299e11528e3e716cc6e1fc88f2a86bff5018a95eb821fdb9d66c92e34ab0c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ipsmobile::Application.config.session_store :cookie_store, key: '_ipsmobile_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ipsmobile::Application.config.session_store :active_record_store
